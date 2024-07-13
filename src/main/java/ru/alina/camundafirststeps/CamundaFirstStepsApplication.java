a1de82e1e02fdf8260199cf5bbea1231d05b7c90
alina-gasprom <alina.k.91@icloud.com>
2024-07-13 22:58:57 +0300

    start

---
+++ src/main/java/ru/alina/camundafirststeps/CamundaFirstStepsApplication.java
@@ -0,0 +1,13 @@
+package ru.alina.camundafirststeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaFirstStepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaFirstStepsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alina/camundafirststeps/CamundaFirstStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alina.camundafirststeps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaFirstStepsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
